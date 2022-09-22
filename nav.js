5e1c1a17b025ce8f3e45d2af579ca1c4c60f70d8
abhayashilpakar <shilpakar567@gmail.com>
2022-09-22 19:35:12 +0545

    sdas

--- nav.js
+++ nav.js
@@ -1,16 +1,24 @@
-const Nav = document.querySelector(".navigation-menu-bar");
-const navHamburger = document.querySelector(".mobile-nav-button");
+// const Nav = document.querySelector(".navigation-menu-bar");
+// const navHamburger = document.querySelector(".mobile-nav-button");
 
-navHamburger.addEventListener('click', () => {
-const hellboy = Nav.getAttribute("data-visible");
+// navHamburger.addEventListener('click', () => {
+// const hellboy = Nav.getAttribute("data-visible");
 
-if (hellboy === "false") {
-    Nav.setAttribute("data-visible","true");
-    navHamburger.setAttribute("aria-expanded","true");
-}
-else if (hellboy === "true") {
-    Nav.setAttribute("data-visible","false");
-    navHamburger.setAttribute("aria-expanded","false");
-} 
+// if (hellboy === "false") {
+//     Nav.setAttribute("data-visible","true");
+//     navHamburger.setAttribute("aria-expanded","true");
+// }
+// else if (hellboy === "true") {
+//     Nav.setAttribute("data-visible","false");
+//     navHamburger.setAttribute("aria-expanded","false");
+// } 
     
-});
+// });
+
+const mobileBtn = document.querySelector(".mobile-nav-button");
+const navBar = document.querySelector('.header-secondary');
+
+mobileBtn.addEventListener('click', function(){
+    navBar.classList.toggle('open-nav');
+
+})
